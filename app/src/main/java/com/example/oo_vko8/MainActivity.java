d3478ab3a238dc66b9d4d24241aa1db0dcb9982c
ojegit <ojegit@gmail.com>
2023-08-01 13:48:32 +0300

    upd 1

--- app/src/main/java/com/example/oo_vko8/MainActivity.java
+++ app/src/main/java/com/example/oo_vko8/MainActivity.java
@@ -1,76 +1,56 @@
 package com.example.oo_vko8;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.oo_vko8.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private EditText firstInputTxt;
+    private EditText secondInputTxt;
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        firstInputTxt = findViewById(R.id.textFirstInp);
+        secondInputTxt = findViewById(R.id.textSecondInp);
+        output = findViewById(R.id.textResult);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onBtnClick(View view) {
+        int id = view.getId();
+        int result = 0;
+        /*
+        firstInputTxt = findViewById(R.id.textFirstInp);
+        secondInputTxt = findViewById(R.id.textSecondInp);
+        output = findViewById(R.id.textResult);
+        */
+        //read inputs
+        int firstInput = Integer.parseInt(String.valueOf(firstInputTxt.getText()));
+        int secondInput = Integer.parseInt(String.valueOf(secondInputTxt.getText()));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        System.out.println("CLICKED: " +firstInput +", "+secondInput);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //check operator
+        if (id == R.id.btnPlus) {
+            result = firstInput + secondInput;
+        } else if (id == R.id.btnMinus) {
+            result = firstInput - secondInput;
+        } else if (id == R.id.btnDiv) {
+            result = firstInput / secondInput;
+        } else if (id == R.id.btnMultiply) {
+            result = firstInput * secondInput;
         }
 
-        return super.onOptionsItemSelected(item);
+        //write result
+        //Note: apparently a specific integer to string conversion is required in order for this to work!
+        output.setText(Integer.toString(result));
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
